ec0821212cfc2e6dfc90a222e8a15be0eab2fab7
NingAnMe <ninganme@qq.com>
2019-02-21 17:09:36 +0800

    面试题

--- creat_timeseries_data.py
+++ creat_timeseries_data.py
@@ -4,3 +4,40 @@
 @Time    : 2019/2/19
 @Author  : AnNing
 """
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import numpy as np
+import pandas as pd
+
+
+def create_timeseries_data():
+    date_start = datetime.now() - relativedelta(years=5)
+    ints = np.random.randint(1, 3, 1000)
+    l1 = list()
+    date_tmp = None
+    for i in ints:
+        if date_tmp is None:
+            date_tmp = date_start
+        date_tmp = date_tmp + relativedelta(days=int(i))
+        l1.append(date_tmp.strftime('%Y%m%d'))
+    data1 = ints
+
+    ints = np.random.randint(1, 3, 1000)
+    date_tmp = None
+    l2 = list()
+    for i in ints:
+        if date_tmp is None:
+            date_tmp = date_start
+        date_tmp = date_tmp + relativedelta(days=int(i))
+        l2.append(date_tmp.strftime('%Y%m%d'))
+    data2 = ints
+
+    df1 = pd.DataFrame({'date': l1, 'value': data1})
+    df2 = pd.DataFrame({'date': l2, 'value': data2})
+
+    df1.to_csv('test_data/data1.xsl', index=False)
+    df2.to_csv('test_data/data2.xsl', index=False)
+
+
+if __name__ == '__main__':
+    create_timeseries_data()
--- plot_sum.py
+++ plot_sum.py
@@ -1,6 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-@Time    : 2019/2/19
-@Author  : AnNing
+编写脚本
+脚本运行输入： dir_path: (str)  例如： python plot_sum.py test_data
+
+脚本功能：
+1、通过输入的目录，读取 data1 和 data2 的数据，数据格式为 date 和 value, 且 date 已经排序
+2、将data1和data2中具有相同 date 的数据进行求和
+3、绘制折线图，x 为 date， y 为 sum
+
+样例：
+数据集1
+date value
+20140220,1
+20140223,2
+20140224,1
+
+数据集2
+date value
+20140220,1
+20140221,1
+20140224,1
+
+结果
+date value
+20140220,2
+20140224,2
+
 """
+import os
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    argv = sys.argv
+    main()
